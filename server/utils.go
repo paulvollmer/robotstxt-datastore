9d0f87aafd8d51a5b3b63b6e5d317e1e46a71db7
Paul Vollmer <paul.vollmer@protonmail.com>
2020-04-19 21:10:18 +0200

    changes at MapDBtoProtoResponse return, created buildRobotstxtURL and isHeaderContentTypeTextPlain funcs

--- server/utils.go
+++ server/utils.go
@@ -5,13 +5,31 @@
 package main
 
 import (
+	"net/http"
 	"strings"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/paulvollmer/robotstxt"
 	pb "github.com/paulvollmer/robotstxt-datastore/proto/robotstxt"
 	"github.com/paulvollmer/robotstxt-datastore/server/ent"
 )
+
+func buildRobotstxtURL(host string) string {
+	return cfg.DefaultRequestScheme + "://" + host + "/robots.txt"
+}
+
+// we do not use http.header.Get becasue we want to check case insensitive
+func isHeaderContentTypeTextPlain(header http.Header) bool {
+	for k, v := range header {
+		if strings.ToLower(k) == "content-type" {
+			if strings.Contains(strings.Join(v, " "), "text/plain") {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 // MapDBtoProtoResponse maps the database model to the protpbuf Robotstxt object
 func MapDBtoProtoResponse(data *ent.Robotstxt) (*pb.Robotstxt, error) {
@@ -24,26 +42,7 @@ func MapDBtoProtoResponse(data *ent.Robotstxt) (*pb.Robotstxt, error) {
 		return nil, err
 	}
 
-	robotstxtParsed, err := robotstxt.FromBytes(data.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	groups := robotstxtParsed.Groups
-
-	rules := make([]*pb.Rule, 0)
-	for k := range groups {
-		paths := make([]string, 0)
-		for i := 0; i < len(groups[k].Rules); i++ {
-			trimmedPath := strings.TrimSpace(groups[k].Rules[i].Path)
-			if trimmedPath != "" {
-				paths = append(paths, trimmedPath)
-			}
-		}
-		rules = append(rules, &pb.Rule{Agent: k, Paths: paths})
-	}
-
-	return &pb.Robotstxt{
+	returnData := pb.Robotstxt{
 		CreatedAt:    createdAt,
 		UpdatedAt:    updatedAt,
 		Host:         data.Host,
@@ -52,7 +51,39 @@ func MapDBtoProtoResponse(data *ent.Robotstxt) (*pb.Robotstxt, error) {
 		ResponseUrl:  data.ResponseURL,
 		ResponseTime: data.ResponseTime,
 		Robotstxt:    string(data.Body),
-		Rules:        rules,
-		Sitemaps:     robotstxtParsed.Sitemaps,
-	}, nil
+	}
+
+	if len(data.Body) == 0 {
+		return &returnData, nil
+	}
+
+	robotstxtParsed, err := robotstxt.FromBytes(data.Body)
+	if err != nil {
+		return &returnData, err
+	}
+
+	returnData.Sitemaps = robotstxtParsed.Sitemaps
+
+	rules := make([]*pb.Rule, 0)
+	for k := range robotstxtParsed.Groups {
+		paths := make([]string, 0)
+		for i := 0; i < len(robotstxtParsed.Groups[k].Rules); i++ {
+			trimmedPath := strings.TrimSpace(robotstxtParsed.Groups[k].Rules[i].Path)
+			if trimmedPath != "" {
+				paths = append(paths, trimmedPath)
+			}
+		}
+		rules = append(rules, &pb.Rule{Agent: k, Paths: paths})
+	}
+	returnData.Rules = rules
+
+	return &returnData, nil
+}
+
+func sentryCaptureException(err error, host, kind string) {
+	sentry.ConfigureScope(func(scope *sentry.Scope) {
+		scope.SetTag("host", host)
+		scope.SetTag("kind", kind)
+	})
+	sentry.CaptureException(err)
 }
